54c09e0fd22c32ce21e7e6023d999f6fb5d079bd
Oksana Kravchenko <oksik17let@gmail.com>
2023-05-21 20:22:52 +0200

    Result

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr = [(-b / (2 * a))];
+  } else if (discriminant > 0) {
+    let x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+    let x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+    arr = [x1, x2];
+  } else{
+    arr = [];
+  };
+
+  return arr; 
 }
+console.log(solveEquation(5, 12, 36));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  };
+
+  let totalAmount;
+  let percentMonths = percent / 100 / 12;
+  let bodyCredit = amount - contribution;
+  let pay = bodyCredit * (percentMonths + (percentMonths / (((1 + percentMonths)**countMonths) - 1)));
+  totalAmount = pay * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
+console.log(calculateTotalMortgage(10, 1000, 50000, 12));
